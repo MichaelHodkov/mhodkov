b529cca00084025d5ec48543becb85e03072b201
MichaelHodkov <happymike19@gmail.com>
2017-12-04 19:59:56 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -3,7 +3,7 @@ package ru.job4j;
 * Class Класс для выводы строки на консоль.
 * @author Michael Hodkov
 */
-public class Calculate{
+public class Calculate {
 	/**
 	* Method echo.
 	* @param name Your name.
@@ -17,7 +17,7 @@ public class Calculate{
 	* Конструктор, вывод строки в консоль.
 	* @param args - args
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello world.");
 	}
 }
