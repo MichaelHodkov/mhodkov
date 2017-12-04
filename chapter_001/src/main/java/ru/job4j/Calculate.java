81e4c838ec23d0108ced0fc571d4115c55ae08ed
MichaelHodkov <happymike19@gmail.com>
2017-12-04 19:03:28 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,13 @@
 package ru.job4j;
-
+/**
+* Class Класс для выводы строки на консоль.
+* @author Michael Hodkov
+*/
 public class Calculate{
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param args - args
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world.");
 	}
