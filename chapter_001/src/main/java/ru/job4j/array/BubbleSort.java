684f660e3a5849b20c631ba3663d82f0b36be446
MichaelHodkov <happymike19@gmail.com>
2017-12-13 01:27:59 +0300

    Edit for Audit CheckStyle

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -12,11 +12,11 @@ public class BubbleSort {
      * @param array - массив для сортировки.
      * @return - отсортированный массив.
      */
-    public int[] sort(int[] array){
+    public int[] sort(int[] array) {
         int temp;
         for (int j = 0; j < array.length; j++) {
             for (int i = 0; i < array.length; i++) {
-                if ( i + 1 < array.length && array[i] > array[i + 1]){
+                if (i + 1 < array.length && array[i] > array[i + 1]) {
                     temp = array[i + 1];
                     array[i + 1] = array[i];
                     array[i] = temp;
