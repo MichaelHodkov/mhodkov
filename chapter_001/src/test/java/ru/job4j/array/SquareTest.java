e136f750fcc037fbdbfc43cdd8790cec452fee5b
MichaelHodkov <happymike19@gmail.com>
2017-12-12 21:30:21 +0300

    дописал автора)

--- chapter_001/src/test/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * @author Michael Hodkov
+ * @version $Id$
+ * @since 0.1
+ */
 public class SquareTest {
 
     @Test
