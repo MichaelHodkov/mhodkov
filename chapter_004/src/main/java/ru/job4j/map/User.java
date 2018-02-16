55e708858535625c40ab678e97aace5e57d62cf8
MichaelHodkov <happymike19@gmail.com>
2018-02-16 11:21:01 +0300

    1. Создать модель User [#999]

---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,20 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * @author Michael Hodkov
+ * @version $Id$
+ * @since 0.1
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
