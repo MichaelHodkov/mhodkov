17ebca92f2710089e49d797ddc4cdc425e7ee2fb
MichaelHodkov <happymike19@gmail.com>
2018-02-16 14:40:45 +0300

    переписал hashnext в один ретурн

--- chapter_004/src/main/java/ru/job4j/iterator/TwoDimensionalArrayIterator.java
+++ chapter_004/src/main/java/ru/job4j/iterator/TwoDimensionalArrayIterator.java
@@ -24,16 +24,20 @@ public class TwoDimensionalArrayIterator implements Iterator {
      *
      * @return {@code true} if the iteration has more elements
      */
+//    @Override
+//    public boolean hasNext() {
+//        boolean flagNextItem = false;
+//        if (values.length == 0) {
+//            return false;
+//        }
+//        if (indexX == values[indexY].length && indexY < values.length - 1) {
+//            return true;
+//        }
+//        return values.length > indexY && values[indexY].length > indexX;
+//    }
     @Override
     public boolean hasNext() {
-        boolean flagNextItem = false;
-        if (values.length == 0) {
-            return false;
-        }
-        if (indexX == values[indexY].length && indexY < values.length - 1) {
-            return true;
-        }
-        return values.length > indexY && values[indexY].length > indexX;
+        return (values.length > 0 && (indexY < values.length - 1 || indexX < values[indexY].length));
     }
 
     /**
