e0ff7ca96a153957e5329096c55f89f0c4370bce
MichaelHodkov <happymike19@gmail.com>
2018-03-05 19:07:12 +0300

    Переписал проверку кто держит lock

--- chapter_005/src/main/java/ru/job4j/wait/MyLock.java
+++ chapter_005/src/main/java/ru/job4j/wait/MyLock.java
@@ -1,33 +1,71 @@
 package ru.job4j.wait;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
 /**
  * @author Michael Hodkov
  * @version $Id$
  * @since 0.1
  */
+@ThreadSafe
 public class MyLock {
+    @GuardedBy("this")
     private final Object lock = new Object();
     private boolean freeLock = true;
+    private String nameThreadLock;
 
     public void getLock() {
-        synchronized (lock) {
-            if (freeLock) {
+        if (freeLock) {
+            synchronized (lock) {
                 freeLock = false;
-                try {
-                    lock.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                System.out.println(String.format("%s i'm lock : lock", Thread.currentThread().getName()));
+                nameThreadLock = Thread.currentThread().getName();
             }
+        } else {
+            System.out.println(String.format("%s not free lock!", Thread.currentThread().getName()));
         }
     }
 
     public void unlock() {
         synchronized (lock) {
-            if (!freeLock) {
+            if (nameThreadLock.equals(Thread.currentThread().getName())) {
+                System.out.println(String.format("%s i'm unlock : lock", Thread.currentThread().getName()));
                 freeLock = true;
-                lock.notify();
+            } else {
+                System.out.println(String.format("%s not my lock", Thread.currentThread().getName()));
             }
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MyLock myLock = new MyLock();
+        Thread threadOne = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                myLock.getLock();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                myLock.unlock();
+            }
+        });
+        Thread threadTwo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                myLock.getLock();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                myLock.unlock();
+            }
+        });
+        threadOne.start();
+        threadTwo.start();
+        Thread.sleep(5000);
     }
 }
