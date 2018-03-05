4adb7f52c0809b1a9a5efd4707eff6f5c7d82d4d
MichaelHodkov <happymike19@gmail.com>
2018-03-05 20:25:46 +0300

    переписал метод изменения значений.

--- chapter_005/src/main/java/ru/job4j/nonblocking/NonBlockingCash.java
+++ chapter_005/src/main/java/ru/job4j/nonblocking/NonBlockingCash.java
@@ -2,6 +2,7 @@ package ru.job4j.nonblocking;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
 
 /**
  * @author Michael Hodkov
@@ -12,38 +13,38 @@ public class NonBlockingCash {
     private ConcurrentHashMap<Integer, User> map = new ConcurrentHashMap<>();
 
     public void add(Integer key, User element) {
-        map.put(key, element);
+        this.map.put(key, element);
     }
 
     public void update(Integer key, User element) throws OplimisticException {
-        User user = getElement(key);
-        if (user != null) {
-            int version = user.getVersion();
-            if (!user.getName().equals(element.getName())) {
-                if (version != user.getVersion()) {
-                    throw new  OplimisticException("Oplimistic exception");
+        this.map.computeIfPresent(key, new BiFunction<Integer, User, User>() {
+            @Override
+            public User apply(Integer integer, User user) {
+                if (element.getVersion() == user.getVersion()) {
+                    user.reName(element.getName());
+                    user.setValue(element.getValue());
+                } else {
+                    try {
+                        throw new OplimisticException("Oplimistic exception");
+                    } catch (OplimisticException e) {
+                        e.printStackTrace();
+                    }
                 }
-                user.reName(element.getName());
+                return user;
             }
-            if (!user.getValue().equals(element.getValue())) {
-                if (version != user.getVersion()) {
-                    throw new  OplimisticException("Oplimistic exception");
-                }
-                user.setValue(element.getValue());
-            }
-        }
+        });
     }
 
     public void delete(Integer key) {
-        map.remove(key);
+        this.map.remove(key);
     }
 
     public User getElement(Integer key) {
-        return map.get(key);
+        return this.map.get(key);
     }
 
     public void seeAll() {
-        for (Map.Entry<Integer, User> entry : map.entrySet()) {
+        for (Map.Entry<Integer, User> entry : this.map.entrySet()) {
             System.out.println(String.valueOf(entry.getKey()).concat(" ").concat(String.valueOf(entry.getValue())));
         }
     }
