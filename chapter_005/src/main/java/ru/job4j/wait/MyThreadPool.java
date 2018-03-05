53120400f5ae73612b3550137cbfd671f86a1326
MichaelHodkov <happymike19@gmail.com>
2018-03-05 18:24:45 +0300

    Переписал под корректную работу нескольких потоков.

--- chapter_005/src/main/java/ru/job4j/wait/MyThreadPool.java
+++ chapter_005/src/main/java/ru/job4j/wait/MyThreadPool.java
@@ -1,5 +1,8 @@
 package ru.job4j.wait;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
 import java.util.LinkedList;
 
 /**
@@ -7,17 +10,27 @@ import java.util.LinkedList;
  * @version $Id$
  * @since 0.1
  */
+@ThreadSafe
 public class MyThreadPool {
     private final MyThread[] threads;
+    @GuardedBy("this")
     private final LinkedList<Work> queueWork = new LinkedList<>();
 
     public MyThreadPool() {
         int cores = Runtime.getRuntime().availableProcessors();
         threads = new MyThread[cores];
+    }
+
+    public void start() {
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new MyThread();
             threads[i].start();
+        }
+    }
 
+    public void stop() {
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].interrupt();
         }
     }
 
@@ -31,17 +44,15 @@ public class MyThreadPool {
     class MyThread extends Thread {
         @Override
         public void run() {
-            synchronized (queueWork) {
-                while (true) {
-                    if (queueWork.size() > 0) {
-                        queueWork.poll().run();
-                    } else {
-                        try {
-                            queueWork.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+            while (!isInterrupted()) {
+                Work work = null;
+                if (queueWork.size() > 0) {
+                    synchronized (queueWork) {
+                        work = queueWork.poll();
                     }
+                }
+                if (work != null) {
+                    work.run();
                 }
             }
         }
@@ -50,12 +61,29 @@ public class MyThreadPool {
     static class Work implements Runnable {
         @Override
         public void run() {
-            System.out.println("Todo...");
+            System.out.println(String.format("Work do thread: %s", Thread.currentThread().getName()));
+            int start = (int) (Math.random() * 100);
+            int stop = start * start * start;
             int sum = 0;
-            for (int i = 0; i < 100000; i++) {
+            for (int i = start; i < stop; i++) {
                 sum += i;
             }
-            System.out.println(String.format("Sum = %d", sum));
+            System.out.println(String.format("%s end... Start[%d], stop[%d], Sum = %d",
+                    Thread.currentThread().getName(), start, stop, sum));
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MyThreadPool pool = new MyThreadPool();
+        pool.add(new Work());
+        pool.add(new Work());
+        pool.add(new Work());
+        pool.add(new Work());
+        pool.add(new Work());
+        pool.add(new Work());
+        pool.add(new Work());
+        pool.start();
+        Thread.sleep(5000);
+        pool.stop();
     }
 }
