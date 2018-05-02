13df616c1879f08270fc1c8026a242ca8186340b
MichaelHodkov <happymike19@gmail.com>
2018-05-02 11:53:23 +0300

    рефакторинг + новая часть

---
+++ chapter_010/src/main/java/ru/job4j/inputandoutput/CheckByteInputStream.java
@@ -0,0 +1,17 @@
+package ru.job4j.inputandoutput;
+
+import java.io.InputStream;
+
+/**
+ * @author Michael Hodkov
+ * @version $Id$
+ * @since 0.1
+ */
+public class CheckByteInputStream {
+
+    //метод должен проверить. что в байтовом потоке записано четное число.
+    public boolean isNumber(InputStream in) {
+        in.
+        return false;
+    }
+}
