17c7d0d40ec315d1c46d47d1d30dda6acf27739d
MichaelHodkov <happymike19@gmail.com>
2018-05-02 13:07:49 +0300

    1. Проверить байтовый поток [#858]

--- chapter_010/src/main/java/ru/job4j/inputandoutput/CheckByteInputStream.java
+++ chapter_010/src/main/java/ru/job4j/inputandoutput/CheckByteInputStream.java
@@ -1,5 +1,9 @@
 package ru.job4j.inputandoutput;
 
+import org.apache.log4j.Logger;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -8,10 +12,46 @@ import java.io.InputStream;
  * @since 0.1
  */
 public class CheckByteInputStream {
+    private static final Logger LOG = Logger.getLogger(CheckByteInputStream.class);
 
     //метод должен проверить. что в байтовом потоке записано четное число.
     public boolean isNumber(InputStream in) {
-        in.
-        return false;
+        BufferedInputStream bufferedInputStream = null;
+        ByteArrayOutputStream byteArrayOutputStream = null;
+        boolean result = false;
+        try {
+            bufferedInputStream = new BufferedInputStream(in);
+            byteArrayOutputStream = new ByteArrayOutputStream();
+            int stream = bufferedInputStream.read();
+            while(stream != -1) {
+                byteArrayOutputStream.write((byte) stream);
+                stream = bufferedInputStream.read();
+            }
+            try {
+                if (Integer.parseInt(byteArrayOutputStream.toString()) % 2 == 0) {
+                    result = true;
+                }
+            } catch (NumberFormatException e) {
+                LOG.info("Stream not number", e);
+            }
+        } catch (IOException e) {
+            LOG.error("Error: read input stream", e);
+        } finally {
+            if (bufferedInputStream != null) {
+                try {
+                    bufferedInputStream.close();
+                } catch (IOException e) {
+                    LOG.error("Error: close buffered input stream", e);
+                }
+            }
+            if (byteArrayOutputStream != null) {
+                try {
+                    byteArrayOutputStream.close();
+                } catch (IOException e) {
+                    LOG.error("Error: close byte array output stream", e);
+                }
+            }
+        }
+        return result;
     }
 }
