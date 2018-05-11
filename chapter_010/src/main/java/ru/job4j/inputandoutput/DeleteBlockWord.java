93ffe485a94000186ef7e0c2df7ecf66337ab240
MichaelHodkov <happymike19@gmail.com>
2018-05-11 12:38:14 +0300

    переписал методы в классе, сделал их публичными. Написал тесты на все методы.

--- chapter_010/src/main/java/ru/job4j/inputandoutput/DeleteBlockWord.java
+++ chapter_010/src/main/java/ru/job4j/inputandoutput/DeleteBlockWord.java
@@ -1,7 +1,7 @@
 package ru.job4j.inputandoutput;
 
 import org.apache.log4j.Logger;
-import java.io.ByteArrayOutputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -21,44 +21,39 @@ public class DeleteBlockWord {
     //Ннельзя зачитать весь поток в память, удалить слова и потом записать.
     //Нужно все делать в потоке.
     public void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
-        try {
-            int read = 0;
-            if (out != null) {
-                out.flush();
-            } else {
-                out = new ByteArrayOutputStream();
-            }
+        try (InputStream inputStream = in;
+        OutputStream outputStream = out
+        ) {
+            int read;
             List<Integer> list = new ArrayList<>();
-            while ((read = in.read()) != -1) {
+            while ((read = inputStream.read()) != -1) {
                 if (read == 32) {
-                    if (!list.isEmpty()) {
-                        work(list, out, abuse);
-                        list.clear();
-                    }
-                    out.write(read);
+                    work(list, outputStream, abuse);
+                    outputStream.write(read);
                 } else {
                     list.add(read);
                 }
             }
-            if (!list.isEmpty()) {
-                work(list, out, abuse);
-                list.clear();
-            }
+            work(list, outputStream, abuse);
         } catch (IOException e) {
-            LOG.error("Error: read input stream", e);
+            LOG.error("Error: ", e);
         }
     }
 
+    //если список не пустой, преобразует байты в слово и
     //записывает слово в выходной поток, если оно не в черном списке.
-    private void work(List<Integer> list, OutputStream out, String[] blackList) throws IOException {
-        String word = getWord(list);
-        if (isGoodWord(word, blackList)) {
-            out.write(word.getBytes());
+    public void work(List<Integer> list, OutputStream out, String[] blackList) throws IOException {
+        if (!list.isEmpty()) {
+            String word = getWord(list);
+            if (isGoodWord(word, blackList)) {
+                out.write(word.getBytes());
+            }
+            list.clear();
         }
     }
 
     //проверяет вхождения слова в черный список
-    private boolean isGoodWord(String word, String[] blackList) {
+    public boolean isGoodWord(String word, String[] blackList) {
         for (String blockWord: blackList) {
             if (blockWord.equals(word)) {
                 return false;
@@ -68,7 +63,7 @@ public class DeleteBlockWord {
     }
 
     //метод перевода списка int в слово.
-    private String getWord(List<Integer> list) {
+    public String getWord(List<Integer> list) {
         StringBuilder word = new StringBuilder();
         for (Integer value: list) {
             word.append(Character.toChars(value));
--- chapter_010/src/test/java/ru/job4j/inputandoutput/DeleteBlockWordTest.java
+++ chapter_010/src/test/java/ru/job4j/inputandoutput/DeleteBlockWordTest.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -19,11 +23,45 @@ public class DeleteBlockWordTest {
     DeleteBlockWord deleteBlockWord = new DeleteBlockWord();
 
     @Test
+    public void whenTestGetWordOfBytes() {
+        String text = "Test";
+        byte[] bytes = text.getBytes();
+        List<Integer> list = new ArrayList<>();
+        for (byte value: bytes) {
+            list.add((int) value);
+        }
+        String expect = deleteBlockWord.getWord(list);
+        assertEquals(text, expect);
+    }
+
+    @Test
+    public void whenTestMetodSearchWordInBlackList() {
+        String[] blackList = {"Tost"};
+        assertTrue(deleteBlockWord.isGoodWord("Test", blackList));
+        assertFalse(deleteBlockWord.isGoodWord("Tost", blackList));
+    }
+
+    @Test
+    public void whenGetNewWordOfBytesAndAddInOutputStream() throws IOException {
+        String text = "Test";
+        byte[] bytes = text.getBytes();
+        List<Integer> list = new ArrayList<>();
+        for (byte value: bytes) {
+            list.add((int) value);
+        }
+        String[] blackList = {"Tost"};
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        deleteBlockWord.work(list, byteArrayOutputStream, blackList);
+        String  expect = byteArrayOutputStream.toString();
+        assertThat(expect, is(text));
+    }
+
+    @Test
     public void whenTestBlockListInInputAndOutputStream() {
         ByteArrayInputStream testIn = new ByteArrayInputStream(new String("Test tost cust").getBytes());
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         String[] strings = {"tost"};
         deleteBlockWord.dropAbuses(testIn, byteArrayOutputStream, strings);
-        System.out.println(byteArrayOutputStream);
+        assertThat(byteArrayOutputStream.toString(), is("Test  cust"));
     }
 }
